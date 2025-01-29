2aca60ab0dafbf38f83bc8ec5cdb371ef0a09663
Tronki97 <alessandro.roncagli3@studio.unibo.it>
2025-01-29 19:14:17 +0100

    added the login_registration path

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import WelcomeView from '../views/WelcomeView.vue'
+import WelcomeView from '../views/Login_Registration.vue'
 import HomeView from '../views/HomeView.vue'
 import CalendarView from '../views/CalendarView.vue'
 import NoteView from '../views/NoteView.vue'
 
 const routes = [
-  { path: '/', component: WelcomeView },
+  { path: '/', component: Login_Registration },
   { path: '/home', component: HomeView },
   { path: '/calendar', component: CalendarView },
   { path: '/notes', component: NoteView },
