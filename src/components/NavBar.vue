e46369908ad2a3ab7c512bbf1f44d9594068dfd7
Tronki97 <alessandro.roncagli3@studio.unibo.it>
2025-01-29 16:00:27 +0100

    fixed Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,34 +4,43 @@
 
 
 <template>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="nav m1-auto onRight">
-                    <li class="nav-item">
-                        <RouterLink class="nav-link active" to="/home">Home</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link" to="/note">Notes</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link" to="#">Projects</RouterLink>
-                    </li>
-                </ul>
+    <div class="container bg-danger" >
+        <div class="row">
+            <div class="col-1">
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-expanded="false" aria-controls="collapseWidthExample">
+                    T
+                </button>
             </div>
-           <!-- <img src="https://dummyimage.com/100x100/ff0000/000000.png&text=USSR" width="40px">-->
+
+            <div class="col-9">
+                <div class="collapse collapse-horizontal" id="navbar">
+                    <ul class="nav" style="width: 350px;">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link active" to="/home">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/note">Notes</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="#">Projects</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink class="nav-link" to="#"><img src="https://dummyimage.com/600x400/000/fff&text=P" width="20px"></RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-2">
+                <img src="https://dummyimage.com/100x100/ff0000/000000.png&text=USSR" width="40px">
+            </div>
         </div>
-    </nav>
+    </div>
 </template>
 
 <style scoped>
 .onRight{
     justify-content: center;
-   
-
-
 }
+
+
 </style>
