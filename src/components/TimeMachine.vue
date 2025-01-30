797d11407eb3e05583d9457f2f6b73306a47dd2b
nicolatravaglini <nicola.travaglini3@studio.unibo.it>
2025-01-30 21:49:38 +0100

    feat: add TimeMachine component and integrate with NavBar modal

---
+++ src/components/TimeMachine.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-4" id="staticBackdropLabel">Time Machine</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
