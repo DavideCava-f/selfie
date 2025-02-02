fc49f02eb0427ebc7cc5cb5ddec870371f4e4b7b
tr4v4 <nicola.travaglini3@studio.unibo.it>
2025-02-02 17:42:50 +0100

    db test

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { User, Event, Note } from './schemas.js';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { User, Event, Note } from "./schemas.js";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 global.rootDir = __dirname;
 
 let app = express();
-app.use(express.static(path.join(global.rootDir, 'dist')));
+app.use(express.static(path.join(global.rootDir, "dist")));
 app.use(cors());
 app.use(express.json());
-
 
 // MongoDB & Mongoose
 const dbName = "selfie232465";
 const mongoCredentials = {
-	user: "site232465",
-	pwd: "uP9ohT3a",
-	site: "mongo_site232465"
-}
-const uri = `mongodb://${mongoCredentials.user}:${mongoCredentials.pwd}@${mongoCredentials.site}/${dbName}?retryWrites=true&w=majority&authSource=admin`;
+  user: "site232465",
+  pwd: "uP9ohT3a",
+  site: "mongo_site232465",
+};
+// const uri = `mongodb://${mongoCredentials.user}:${mongoCredentials.pwd}@${mongoCredentials.site}/${dbName}?retryWrites=true&w=majority&authSource=admin`;
+// Atlas dev database
+const uri = `mongodb+srv://nicola1travaglini:testtest@test.pe0yf.mongodb.net/keybench?retryWrites=true&w=majority&appName=Test"`;
 
 // Functions
 
 // Entry points
-app.get('*', (req, res) => {
-    res.sendFile(path.join(global.rootDir, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(global.rootDir, "dist", "index.html"));
 });
 
-app.post('/CreateNote', async function(req, res) {
-    try {
-        await mongoose.connect(uri);
-
-    } finally {
-        mongoose.connection.close();
-    }
+app.post("/CreateNote", async function (req, res) {
+  try {
+    await mongoose.connect(uri);
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
-app.get('/ReadNotes', async function(req, res) {
-    try {
-        await mongoose.connect(uri);
-
-    } finally {
-        mongoose.connection.close();
-    }
+app.get("/ReadNotes", async function (req, res) {
+  try {
+    await mongoose.connect(uri);
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
-app.get('/example', async function(req, res) {
-    try {
-        await mongoose.connect(uri);
-    } finally {
-        mongoose.connection.close();
-    }
+app.get("/example", async function (req, res) {
+  try {
+    await mongoose.connect(uri);
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
-app.get('/dbdebug', async function(req, res) {
-    try {
-        await mongoose.connect(uri);
-        const test = await Note.find({});
-        res.json(test);
-    } finally {
-        mongoose.connection.close();
-    }
+app.get("/dbdebug", async function (req, res) {
+  try {
+    await mongoose.connect(uri);
+    const test = await Note.find({});
+    res.json(test);
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
-app.listen(8000, function() { 
-	global.startDate = new Date() ; 
-	console.log(`App listening on port 8000 started ${global.startDate.toLocaleString()}` )
-})
+app.listen(8000, function () {
+  global.startDate = new Date();
+  console.log(
+    `App listening on port 8000 started ${global.startDate.toLocaleString()}`,
+  );
+});
