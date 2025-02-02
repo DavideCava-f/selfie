0c9bfe766f735c0bb8f81e391cd88126d330df39
tr4v4 <nicola.travaglini3@studio.unibo.it>
2025-02-02 16:54:16 +0100

    formatting

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
 
 // Per debugging, l'app porta subito a CalendarView
-router.push('/calendar');
-
+router.push("/calendar");
 </script>
 
 <template>
-    <RouterView />
+  <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/TimeMachine.vue
+++ src/components/TimeMachine.vue
@@ -1,24 +1,29 @@
 <script setup>
-import { store } from '@/composables/store';
-
+import { store } from "@/composables/store";
 </script>
 
 <template>
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-4" id="staticBackdropLabel">Time Machine</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <button @click="store.deltaTime+=1000">Inc</button>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
-            </div>
-        </div>
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-4" id="staticBackdropLabel">Time Machine</h1>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        <button @click="store.deltaTime += 1000">Inc</button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+          Save changes
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
