9374cab7b5b62f4d932ecc866d9c4d6c7064f4cd
Tronki97 <alessandro.roncagli3@studio.unibo.it>
2025-02-05 17:59:56 +0100

    added dummy login

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,11 +9,92 @@ const currentRoute = computed(() => router.currentRoute.value["path"]);
 
 const corsi = ["informatica", "elettronica", "informatica per il management"];
 
+let UserEx = [{
+    name: "Alessandro",
+    username: "ale",
+    password: "suca",
+    surname: "Roncaglia",
+    email: "aleron04@gmail.com",
+    course: "informatica"
+}, 
+{
+    name: "Nicola",
+    username: "nico",
+    password: "chiara",
+    surname: "Travaglini",
+    email: "nicotra@gmail.com",
+    course: "informatica"
+}];
+
+
+
+
 function validateForm(){
-    
+    let username = document.forms["login"]["username"].value;
+    let pswd = document.forms["login"]["password"].value;
+    let i=0
+    let found = false;
+    while(!found && i < UserEx.length){
+        if((username === UserEx[i].username || username===UserEx[i].email ) && pswd === UserEx[i].password){
+            console.log("Login effettuato");
+            found = true;
+        }
+        i++;
+    }
+
+    if(!found){
+        alert("Username o password errati");
+    }
+    else{
+        router.push('/home');
+    }
 }
 
+function addUser(){
+    let name = document.forms["signup"]["name"].value;
+    let surname = document.forms["signup"]["surname"].value;
+    let username = document.forms["signup"]["username"].value;
+    let course = document.getElementById("corsi").selectedIndex;
+    let y = document.getElementById("corsi").options;
+    let pswd = document.forms["signup"]["pswd"].value;
+    let confirmPswd = document.forms["signup"]["confirmPswd"].value;
+    if(pswd === confirmPswd){
+        
+        fetch("http://localhost:5173/ReadNotes")
 
+
+        fetch('http://localhost:5173/CreateUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                username: username,
+                password: pswd,
+                surname: surname,
+                email: username,
+                course: y[course].text
+            }),
+        })
+        /*
+        UserEx.push({
+            name: name,
+            username: username,
+            password: pswd,
+            surname: surname,
+            email: username,
+            course: y[course].text
+        });
+        */
+        console.log("Utente aggiunto");
+        console.log(UserEx[UserEx.length-1]);
+        //router.push('/login');
+    }
+    else{
+        alert("Le password non coincidono");
+    }
+}
 </script>
 
 
@@ -32,22 +113,22 @@ function validateForm(){
                             style="width: 185px;" alt="logo">
                         </div>
 
-                        <form v-if="currentRoute === '/login'">
+                        <form name="login" v-if="currentRoute === '/login'" onsubmit="validateForm()">
                             <p>Please login to your account</p>
 
                             <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="email" id="form2Example11" class="form-control"
-                                placeholder="Phone number or email address" />
-                                <label class="form-label" for="form2Example11">Username</label>
+                                <label class="form-label" for="username">Username</label>
+                                <input type="email" id="username" class="form-control"
+                                placeholder="Phone number or email address" name="username"/>
                             </div>
 
                             <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="password" id="form2Example22" class="form-control" />
-                                <label class="form-label" for="form2Example22">Password</label>
+                                <label class="form-label" for="password">Password</label>
+                                <input type="password" id="password" class="form-control" name="password"/>
                             </div>
 
                             <div class="text-center pt-1 mb-5 pb-1">
-                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit" @click="validateForm">Log
                                 in</button>
                                 <a class="text-muted" href="#!">Forgot password?</a>
                             </div>
@@ -60,45 +141,52 @@ function validateForm(){
                             </div>
                         </form>
 
-                        <form name="signup" v-if="currentRoute === '/signup'" onsubmit="return validateForm()">
+                        <form name="signup" v-if="currentRoute === '/signup'" class="was-validated" onsubmit="addUser">
                             <p>Create your account</p>
 
                             <div data-mdb-input-init class="form-outline mb-1">
-                                <input type="email" id="form2Example11" class="form-control"/>
-                                <label class="form-label" for="form2Example11">Name</label>
+                                <label class="form-label" for="name">Name</label>
+                                <input type="text" id="name" class="form-control" name="name" required/>
+                                <div class="invalid-feedback">Please choose a name.</div>
+
                             </div>
 
                             <div data-mdb-input-init class="form-outline mb-1">
-                                <input type="email" id="form2Example11" class="form-control"/>
-                                <label class="form-label" for="form2Example11">Surname</label>
+                                <label class="form-label" for="surname">Surname</label>
+                                <input type="text" id="surname" class="form-control" name="surname" required/>
+                                <div class="invalid-feedback">Please choose a surname.</div>
                             </div>
 
                             <div data-mdb-input-init class="form-outline mb-1">
-                                <input type="email" id="form2Example11" class="form-control"
-                                placeholder="Email address" />
                                 <label class="form-label" for="form2Example11">Username</label>
+                                <input type="text" id="username" class="form-control"
+                                placeholder="Email address" name="username" required/>
+                                <div class="invalid-feedback">Please choose a username.</div>
                             </div>
 
-                            <select class="form-select mb-1" aria-label="Programme" id="corsi">
+                            <label class="form-label" for="corsi">Programme</label>
+                            <select class="form-select mb-1" aria-label="Programme" id="corsi" required>
                                 <option selected disabled value="">Choose...</option>
-                                <option v-for="corso in corsi" value=${corso.index} class="text-black">
+                                <option v-for="corso in corsi" class="text-black">
                                     {{ corso }}
                                 </option> 
                             </select>
-                            <label class="form-label" for="corsi">Programme</label>
+                            <div class="invalid-feedback">Please choose a programme.</div>
 
                             <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="password" id="form2Example22" class="form-control" />
-                                <label class="form-label" for="form2Example22">Password</label>
+                                <label class="form-label" for="pswd">Password</label>
+                                <input type="password" id="pswd" class="form-control" required/>
+                                <div class="invalid-feedback">Please choose a password.</div>
                             </div>
 
                             <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="password" id="form2Example22" class="form-control" />
-                                <label class="form-label" for="form2Example22">Confirm password</label>
+                                <label class="form-label" for="confirmPswd">Confirm password</label>
+                                <input type="password" id="confirmPswd" class="form-control" required/>
+                                <div class="invalid-feedback">Please confirm your password.</div>
                             </div>
 
                             <div class="text-center pt-1 mb-5 pb-1">
-                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-danger btn-block fa-lg gradient-custom-2 mb-3" type="submit">Sign up</button>
+                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-danger btn-block fa-lg gradient-custom-2 mb-3" type="submit" @click="addUser">Sign up</button>
                             </div>
 
                             <div class="d-flex align-items-center justify-content-center pb-4">
