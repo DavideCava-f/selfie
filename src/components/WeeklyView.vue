778cfe93e90bd0bbff0ee90dbd7534458a41b003
nicolatravaglini <nicola.travaglini3@studio.unibo.it>
2025-03-18 15:30:01 +0100

    init

---
+++ src/components/WeeklyView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from "vue";
+
+</script>
+
+
+<template>
+  <div class="container d-flex flex-column justify-content-between h-100 w-100">
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+    <div class="row w-100 p-3">
+      <div class="col-1 h-100">
+        <div class="fw-bold fs-3 text-white">MON</div>
+      </div>
+      <div class="col-11 h-100 d-flex flex-row justify-content-between"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
