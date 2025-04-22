22e8301dffff3945ca0c58f637f6a61a4741429b
Tronki97 <alessandro.roncagli3@studio.unibo.it>
2025-04-22 17:29:42 +0200

    Ristruttura il layout in HomeView.vue per una migliore visualizzazione degli eventi e delle note, utilizzando componenti card e migliorando la disposizione degli elementi.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import { useRouter, useRoute } from "vue-router";
 let lastnote = ref({});
 let nearEvents = ref([]);
 let loaded = ref(Boolean);
-let progetti = ref({});
+let pomodoro = ref({});
 const router = useRouter();
 
 
@@ -75,37 +75,53 @@ function getVisibleDate(date) {
     <NavBar />
     <div class="container-fluid">
         <div class="row justify-content-center p-3 " >
-            <div class="col-lg-3 col-12 ">
-                <div class="align-items-center">
-                    <h2 class="mx-auto">Eventi prossimi</h2>
-                </div>
-                <div class="container-fluid bg-danger d-flex flex-column overflow-scroll rounded-4 overflow-x-hidden align-items-center"
-                    style="max-height: 80vh;">
-                    <div v-if="nearEvents.length == 0">
-                        <h1>Non ci sono eventi prossimi</h1>
-                    </div>
-                    <div v-else>
-                        <button v-for="event in nearEvents" @click="router.push('/calendar')"
-                            class="w-100 p-0 btn bg-success rounded-3 text-black m-2 flex-fill">
-                            <h2>{{ event.title }}</h2>
-                            {{ event.details.text }}
-                            <h3>ripetizioni:</h3>
-                            <div v-for="date in event.dates">
-                                <p>{{ getVisibleDate(date.begin) }}</p>
-                                <p>{{ getVisibleDate(date.end) }}</p>
-                            </div>
-                        </button>
-                    </div>
+            <div class="col-lg-3 col-12 "><!-- colonna prossimi eventi -->
 
+                <div class="card text-bg-danger mb-3" style="max-height: 80vh;">
+                    <div class="card-header align-items-center">
+                        <h2 class="mx-auto">Eventi prossimi</h2>
+                    </div>
+                    <div class="card-body overflow-scroll rounded-4 overflow-x-hidden align-items-center">
+                        <div v-if="nearEvents.length == 0">
+                            <h1>Non ci sono eventi prossimi</h1>
+                        </div>
+                        <div v-else>
+                            <button v-for="event in nearEvents" @click="router.push('/calendar')"
+                                class="w-100 btn bg-success rounded-3 text-black my-1 justify-content-start">
+                                <h2>{{ event.title }}</h2>
+                                {{ event.details.text }}
+                                <h3>ripetizioni:</h3>
+                                <table class="table-success ">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">Start</th>
+                                        <th scope="col">End</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="date in event.dates" :key="date.begin">
+                                        <td>{{ getVisibleDate(date.begin) }}</td>
+                                        <td>{{ getVisibleDate(date.end) }}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <!-- <div v-for="date in event.dates">
+                                    <p>{{ getVisibleDate(date.begin) }}</p>
+                                    <p>{{ getVisibleDate(date.end) }}</p>
+                                </div> -->
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-12">
-                <div class="align-items-center">
-                    <h2 class="mx-auto">Ultima nota modificata</h2>
-                </div>
-                <div @click="gotoNote" class="container-fluid bg-danger d-flex flex-column overflow-scroll rounded-4"
-                    style="max-height: 80vh">
-                    <button class="btn bg-success rounded-3 text-black m-2">
+            <div class="col-lg-3 col-12"><!-- colonna ultima nota -->
+
+                <div class="card text-bg-danger mb-3" style="max-height: 80vh;">
+                    <div class="card-header align-items-center">
+                        <h2 class="mx-auto">Ultima nota modificata</h2>
+                    </div>
+                    <div class="card-body overflow-scroll rounded-4 overflow-x-hidden align-items-center">
+                        <button @click="gotoNote" class="btn bg-success rounded-3 text-black m-2">
                         <h1 v-if="loaded">
                             {{ lastnote.Title }}
                         </h1>
@@ -120,24 +136,40 @@ function getVisibleDate(date) {
                         </p>
 
                     </button>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-3 col-12">
-                <div class="align-items-center">
-                    <h2 class="mx-auto">Progetti in corso</h2>
-                </div>
-                <div class="container-fluid bg-danger d-flex flex-column overflow-scroll rounded-4"
-                    style="max-height: 80vh">
-                    <div class="bg-success rounded-3 text-black m-2">
-                        <div v-if="progetti.value">
+                <div class="card text-bg-danger mb-3" style="max-height: 80vh;">
+                    <div class="card-header align-items-center">
+                        <h2 class="mx-auto">Progetti in corso</h2>
+                    </div>
+                    <div class="card-body overflow-scroll rounded-4 overflow-x-hidden align-items-center">
+                        <div v-if="pomodoro.value">
 
                         </div>
                         <div v-else>
                             Non ci sono progetti in corso
                         </div>
-
                     </div>
                 </div>
+
+
+                <!-- <div class="align-items-center">
+                    <h2 class="mx-auto">Report ultimo pomodoro</h2>
+                </div>
+                <div class="container-fluid bg-danger d-flex flex-column overflow-scroll rounded-4"
+                    style="max-height: 80vh">
+                    <div class="bg-success rounded-3 text-black m-2">
+                        <div v-if="pomodoro.value">
+
+                        </div>
+                        <div v-else>
+                            Nessun pomodoro fatto
+                        </div>
+
+                    </div>
+                </div> -->
             </div>
         </div>
     </div>
