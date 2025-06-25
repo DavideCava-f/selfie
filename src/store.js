2c4b41b7ed2f669e2e0658c12f1ad2ab279392a3
Tronki97 <alessandro.roncagli3@studio.unibo.it>
2025-06-25 19:28:56 +0200

    Rimuovi la gestione delle lingue dallo store per semplificare la struttura dei dati.

--- src/store.js
+++ src/store.js
@@ -35,12 +35,6 @@ const store = ref({
   realDay: computed(() => store.value.realDate.dayOfWeek - 1),
   simDay: computed(() => store.value.simDate.dayOfWeek - 1),
 
-  lenguages: [
-    "italiano",
-    "english",
-  ],
-  
-  selectedLanguange: "italiano",
 
   week: [
     "Monday",
