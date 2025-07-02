e4ac589d159958f75bb40243d52ed2206033291e
tr4v4 <nicola.travaglini3@studio.unibo.it>
2025-07-02 15:12:20 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { store } from "@/store";
 import { ref, onBeforeMount, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
+import { Temporal } from "@js-temporal/polyfill";
 
 let lastnote = ref({});
 let nearEvents = ref([]);
@@ -25,7 +26,7 @@ async function getLastPomodoro() {
     });
     if (!response.ok) return;
     pomodoro.value = await response.json();
-    console.log(pomodoro.value); 
+    console.log(pomodoro.value);
 }
 
 async function update() {
@@ -73,8 +74,8 @@ function ToEvent(event) {
     const d2 = Temporal.PlainDate.from(event.dates[0].begin.slice(0, 10));
 
     const diff = d2.since(d1);
-    console.log("week offsett:",diff.days%7 );
-    store.value.weekOffset+= Math.floor(diff.days / 7);
+    console.log("week offsett:", diff.days % 7);
+    store.value.weekOffset += Math.floor(diff.days / 7);
     router.push('/calendar');
 }
 
@@ -107,7 +108,7 @@ function ToEvent(event) {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr >
+                                            <tr>
                                                 <td>{{ getVisibleDate(event.dates[0].begin) }}</td>
                                                 <td>{{ getVisibleDate(event.dates[0].end) }}</td>
                                             </tr>
@@ -157,37 +158,47 @@ function ToEvent(event) {
                                 </div>
                                 <div class="col-6 d-flex flex-column align-items-center ">
                                     <h4>
-                                        Studio 
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-                                            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                                        Studio
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                                            <path
+                                                d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
                                         </svg>
                                     </h4>
-                                    <p>{{ pomodoro.studyMins }} {{ pomodoro.studyMins > 1 ? "minuti": "minuto" }}</p>
+                                    <p>{{ pomodoro.studyMins }} {{ pomodoro.studyMins > 1 ? "minuti" : "minuto" }}</p>
                                 </div>
                                 <div class="col-6 d-flex flex-column align-items-center">
                                     <h4>
                                         Pausa
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
-                                            <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
+                                            <path
+                                                d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5" />
                                         </svg>
                                     </h4>
-                                    <p>{{ pomodoro.pauseMins }} {{ pomodoro.pauseMins > 1 ? "minuti": "minuto" }}</p>
+                                    <p>{{ pomodoro.pauseMins }} {{ pomodoro.pauseMins > 1 ? "minuti" : "minuto" }}</p>
                                 </div>
                                 <div class="col-12 d-flex flex-column align-items-center">
                                     <h4>
                                         cicli
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
-                                            <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-arrow-counterclockwise"
+                                            viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z" />
+                                            <path
+                                                d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466" />
                                         </svg>
                                     </h4>
-                                    <p>{{ pomodoro.cycles }} {{ pomodoro.cycles > 1 ? "Cicli": "Ciclo" }}</p>
+                                    <p>{{ pomodoro.cycles }} {{ pomodoro.cycles > 1 ? "Cicli" : "Ciclo" }}</p>
                                 </div>
                                 <div class="col-12 d-flex flex-column align-items-center">
                                     <h4>
                                         Totale pomodoro:
                                     </h4>
-                                    <p>{{ (pomodoro.studyMins + pomodoro.pauseMins)*pomodoro.cycles }} {{ (pomodoro.studyMins + pomodoro.pauseMins)*pomodoro.cycles > 1 ? "minuti": "minuto" }}</p>
+                                    <p>{{ (pomodoro.studyMins + pomodoro.pauseMins) * pomodoro.cycles }} {{
+                                        (pomodoro.studyMins + pomodoro.pauseMins) *pomodoro.cycles > 1 ? "minuti":
+                                        "minuto" }}</p>
                                 </div>
                             </div>
                         </div>
