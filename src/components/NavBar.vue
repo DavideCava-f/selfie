641af9844fd8a346e4c65a72abf9ae4d0024fd06
DavideCava <davide.cavallo2@studio.unibo.it>
2025-07-03 21:34:55 +0200

    2chancemodal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -100,12 +100,39 @@ onMounted(() => setUserPfp());
                     <img :src="pfpSrc" class="rounded dropdown-toggle" width="41vh" data-bs-toggle="dropdown">
                     <ul class="dropdown-menu">
                         <li><button class="dropdown-item" @click="logout">Logout</button></li>
-                        <li><button class="dropdown-item" @click="deleteAccount">Delete account</button></li>
+                        <li><button class="dropdown-item" data-bs-target="#deleteAccountModal" data-bs-toggle="modal">Delete account</button></li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
+        <div class="modal fade" id="deleteAccountModal" data-bs-backdrop="false">
+
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content custom-modal">
+        <div class="modal-header bg-header">
+            <h1 class="modal-title fs-4" id="staticBackdropLabel">
+                Are you sure?
+            </h1>
+
+            <br />
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+        </div>
+        <div class="modal-body">
+            <div class="my-2">
+                <strong style="color:red"> WARNING!!</strong> This action is irreversible
+                <hr>
+
+                <button class="btn btn-danger" @click="deleteAccount" data-bs-dismiss="modal">Delete Account</button>
+            
+            </div>
+            </div>
+
+        </div>
+        </div>
+
+        </div>
 
     <!-- Time Machine modal -->
     <div class="modal fade" id="timeMachineModal" data-bs-backdrop="false" tabindex="-1"
